8ff63b88303e8f5c4becc94240069aae92c88f99
gabriel_rc <ganriel201@gmail.com>
2025-08-23 21:54:00 -0300

    criação da rota de todo

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -12,7 +12,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::json;
 use sqlx::{postgres::PgPoolOptions, PgPool};
 use tokio::net::TcpListener;
-use chrono::{Date, DateTime, NaiveDate, NaiveDateTime, Utc};
+use chrono::{DateTime, NaiveDate, NaiveDateTime, Utc};
 use uuid::Uuid;
 
 #[tokio::main]
@@ -55,7 +55,7 @@ async fn main() {
 }
 
 #[derive(Debug, serde::Serialize, sqlx::Type)]
-#[sqlx(type_name = "EnumStatus", rename_all = "SCREAMING_SNAKE_CASE")]
+#[sqlx(type_name = "\"EnumStatus\"", rename_all = "SCREAMING_SNAKE_CASE")]
 enum EnumStatus {
     TODO,
     IN_PROGRESS,
@@ -63,7 +63,7 @@ enum EnumStatus {
 }
 
 #[derive(Serialize, sqlx::FromRow)]
-struct TodoRow {
+struct TodoRow { // entidade que descreve o dado no banco
     id: Uuid,
     usuarioid: Uuid,
     titulo: String,
@@ -100,10 +100,52 @@ async fn get_todo(
     todo!()
 }
 
+#[derive(Deserialize)]
+struct TodoInput { // objeto que representa a entrada para criação/atualização de uma tarefa
+    titulo: String,
+    descricao: String,
+    praso: NaiveDate,
+}
+
 async fn create_todo(
-    State(pg_pool): State<PgPool>
+    State(pg_pool): State<PgPool>,
+    Json(todo): Json<TodoInput>
 ) -> Result<(StatusCode, String), (StatusCode, String)> {
-    todo!()
+    let usuario_id: Uuid = Uuid::parse_str("141de553-8034-4ed0-86af-28d7bfc5a9a0").unwrap(); // temporário até fazer a autenticação
+
+    let created_todo = sqlx::query_as::<_, TodoRow>(
+        r#"
+        INSERT INTO tarefas (usuarioid, titulo, descricao, praso, status, createdat, updatedat)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING
+            id,
+            usuarioid,
+            titulo,
+            descricao,
+            praso,
+            status,
+            createdat,
+            updatedat
+    "#
+    )
+    .bind(usuario_id)
+    .bind(todo.titulo)
+    .bind(todo.descricao)
+    .bind(todo.praso)
+    .bind(EnumStatus::TODO)
+    .bind(Utc::now())
+    .bind(Utc::now())
+    .fetch_one(&pg_pool)
+    .await
+    .map_err(|e| {(
+        StatusCode::INTERNAL_SERVER_ERROR,
+        json!({"success": false, "message": e.to_string()}).to_string()
+    )})?;
+
+    Ok((
+        StatusCode::CREATED,
+        json!({"success": true, "data": created_todo}).to_string()
+    ))
 }
 
 async fn update_todo(
